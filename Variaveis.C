040961aa75c0e92cf9edd19899afe8202707a811
Rudy <rodolphoar@gmail.com>
2025-05-10 18:59:02 -0300

    Add files via upload

---
+++ Variaveis.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int idade, matricula;
+    float altura;
+    char nome[50];
+
+    printf("digite sua idade: \n");
+    scanf("%d", &36);
+
+    printf("digite sua altura: \n");
+    scanf("%f", &1.86);
+
+    printf("digite seu nome: \n");
+    scanf("%s" , &Rodolpho);
+
+    printf("digite sua matricula: \n");
+    scanf("%d", &0001);
+
+    printf("nome do aluno: %s - matricula: %d \n", Rodolpho, 0001);
+      printf("idade: %d - altura: %f" , 36, 1.86);
+
+    return 0; 
+ 
+}
